573cf5045908130955f787488d8cf5d13d1a2008
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-27 21:02:44 +0530

    Add escape character to reset

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -6,9 +6,9 @@ printf "New line below Hello World"
 #NOTE: The -e option of the echo command enable the parsing of the escape sequences.
 #NOTE: The “\e[0m” sequence removes all attributes (formatting and colors).
 #It can be a good idea to add it at the end of each colored text.
-echo -e "This is \e[1mBold"
-echo -e "This is \e[2mDim"
-echo -e "This is \e[4mUnderline"
-echo -e "This is \e[5mBlink"
-echo -e "This is \e[7mInverted Color"
-echo -e "This is \e[8mHidden"
+echo -e "This is \e[1mBold\e[0m"
+echo -e "This is \e[2mDim\e[0m"
+echo -e "This is \e[4mUnderline\e[0m"
+echo -e "This is \e[5mBlink\e[0m"
+echo -e "This is \e[7mInverted Color\e[0m"
+echo -e "This is \e[8mHidden\e[0m"

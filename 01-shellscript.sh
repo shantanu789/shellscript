c4a2626230ed94c5279b0ff04771995a4b2412dd
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-27 21:12:50 +0530

    add reset code

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -6,9 +6,18 @@ printf "New line below Hello World\n"
 #NOTE: The -e option of the echo command enable the parsing of the escape sequences.
 #NOTE: The “\e[0m” sequence removes all attributes (formatting and colors).
 #It can be a good idea to add it at the end of each colored text.
+#Set of codes
 echo -e "This is \e[1mBold\e[0m"
 echo -e "This is \e[2mDim\e[0m"
 echo -e "This is \e[4mUnderline\e[0m"
 echo -e "This is \e[5mBlink\e[0m"
 echo -e "This is \e[7mInverted Color\e[0m"
 echo -e "This is \e[8mHidden\e[0m"
+
+#Reset of codes
+echo -e "This is \e[1mBold\e[21mNormal\e[0m"
+echo -e "This is \e[2mDim\e[22mNormal\e[0m"
+echo -e "This is \e[4mUnderline\e[24mNormal\e[0m"
+echo -e "This is \e[5mBlink\e[25mNormal\e[0m"
+echo -e "This is \e[7mInverted Color\e[27mNormal\e[0m"
+echo -e "This is \e[8mHidden\e[28mNormal\e[0m"

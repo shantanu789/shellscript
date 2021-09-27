6e75bf6ea05b6055e00c760618720b815d2aefbf
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-27 20:51:16 +0530

    Add Text color demo

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 echo "Hello World"
 printf "New line below Hello World"
+
+echo -e "This is \e[1mBold"
+echo -e "This is \e[2mDim"
+echo -e "This is \e[4mUnderline"
+echo -e "This is \e[5mBlink"
+echo -e "This is \e[7mInverted Color"
+echo -e "This is \e[8mHidden"

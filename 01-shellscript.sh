97e27db2ede53e1edd61eee15c15408187c2df34
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-27 21:00:12 +0530

    Add Text color demo and some notes

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -4,7 +4,7 @@ printf "New line below Hello World"
 #Bash tips: Color and Formatting (ANSI/VT100 Control sequences)
 #Escape character <Esc> "\e","\033","\x1B" and followed by character/color code "<Esc>[FormatCodem..."
 #NOTE: The -e option of the echo command enable the parsing of the escape sequences.
-#NOTE²: The “\e[0m” sequence removes all attributes (formatting and colors).
+#NOTE: The “\e[0m” sequence removes all attributes (formatting and colors).
 #It can be a good idea to add it at the end of each colored text.
 echo -e "This is \e[1mBold"
 echo -e "This is \e[2mDim"

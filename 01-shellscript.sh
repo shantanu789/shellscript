db656698f6f7f2cd8d133df98ff9441f91324b7d
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-28 07:30:50 +0530

    add Foreground text color code

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -21,3 +21,12 @@ echo -e "This is \e[4mUnderline \e[24mNormal\e[0m"
 echo -e "This is \e[5mBlink \e[25m Normal\e[0m"
 echo -e "This is \e[7mInverted Color \e[27mNormal\e[0m"
 echo -e "This is \e[8mHidden \e[28mNormal\e[0m"
+
+#Foreground Text color code
+echo -e "R Red color code 31 \e[31mRed \e[0m"
+echo -e "G Green color code 32 \e[32mGreen \e[0m"
+echo -e "Y Yellow color code 33 \e[33mYellow \e[0m"
+echo -e "B Blue color code 34 \e[34mBlue \e[0m"
+echo -e "M Magenta color code 35 \e[35mMagenta \e[0m"
+echo -e "C Cyan color code 36 \e[36mCyan \e[0m"
+echo -e "Dark Gray color code 37 \e[37Dark Gray \e[0m"

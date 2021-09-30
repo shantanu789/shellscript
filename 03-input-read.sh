528cfba75d04c9787cb36c4e8e56d369d71c3793
Shantanu Unde <er.shantanuu@gmail.com>
2021-09-30 13:37:48 +0530

    Additional script basics and create Roboshop dir

---
+++ 03-input-read.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#Input can be taken by two ways
+#1) During the execution
+#2) Before the execution
+
+#Lets see, example of taking input of During the execution
+
+read -p "Enter your name: " name
+echo "Your name is: ${name}"
---
+++ 04-input-special-variables.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#Positional variable declaration, $0--$n, $*, $@, $#
+echo Script Name = $0
+echo First argumant = $1
+echo Second argumant = $2
+echo All arguments in one string = $*
+echo all arguments in list format = $@
+echo Number of argumant = $#

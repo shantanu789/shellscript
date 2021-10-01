820f836cb6aef97d6c4d5148a74e01cbc93f5b08
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-01 23:36:53 +0530

    add informational messages

--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing MongoDB"
+echo "Set up MongoDB repo"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -9,17 +9,20 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
+echo "Installing MongoDB"
 yum install -y mongodb-org >/tmp/log
 
 #systemctl enable mongod
 #systemctl start mongod
 
 #Update List IP address from 127.0.0.1 to 0.0.0.0 in config file '/etc/mongod.conf'
-
+echo "Modified /etc/mongod.conf"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
+echo "Restart MongoDB"
 systemctl restart mongod
 
+echo "Download the RoboShop schema and load it"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp

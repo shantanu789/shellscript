9a8628791a02cb6a9073c714f7a82aefb8b96270
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-04 23:01:49 +0530

    Commit to Mongodb script

--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -13,7 +13,7 @@ Print(){
   echo -n -e "$1 - \t\t "
 }
 
-Print "Set up MongoDB repo\t\t"
+Print "Set up MongoDB repo\t\t\t\t"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -23,18 +23,18 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 Status_check $?
 
-Print "Installing MongoDB\t\t"
+Print "Installing MongoDB\t\t\t\t"
 yum install -y mongodb-org &>>/tmp/log
 Status_check $?
 #systemctl enable mongod
 #systemctl start mongod
 
 #Update List IP address from 127.0.0.1 to 0.0.0.0 in config file '/etc/mongod.conf'
-Print "Modified /etc/mongod.conf\t"
+Print "Modified /etc/mongod.conf\t\t\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 Status_check $?
 
-Print "Restart MongoDB\t\t"
+Print "Restart MongoDB\t\t\t\t"
 systemctl restart mongod
 Status_check $?
 
@@ -42,12 +42,12 @@ Print "Download the RoboShop schema and load it"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 Status_check $?
 
-Print "Downloaded schema unzipped\t"
+Print "Downloaded schema unzipped\t\t\t"
 cd /tmp
 unzip -o mongodb.zip &>>/tmp/log
 Status_check $?
 
-Print "Downloaded schema loaded\t"
+Print "Downloaded schema loaded\t\t\t"
 cd mongodb-main
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log

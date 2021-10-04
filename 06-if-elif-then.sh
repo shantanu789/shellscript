f0fb6918c53e7ec997f47ca78edfb970e07df1c4
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-04 05:40:05 +0530

    Adding if-else demo script

--- 06-if-elif-then.sh
+++ 06-if-elif-then.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-read -p "Enter your input: " input
+read -r "Enter your input: " input
 
 if [ -z "$input" ]; then
   echo "Please enter the input, You have not enter anything."
-else
-  echo "This is your input: $input"
+  exit 1
 fi
-} while [ -z != $input ]
+
+if [ "$input" == "ABC" ]; then
+  echo "This is your input: ABC"
+fi
+
+echo input: "$input"
+
+if [ "$?" -eq 0 ]; then
+  echo Success
+else
+  echo Failure
+fi
+
+read -p "Enter the filename or file path: " filename
+
+if [ -e "$filename" ]; then
+  echo "File does exists $filename"
+else
+  echo "File does not exists"
+fi

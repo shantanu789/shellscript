b3bf5c1db870141d1f879d8899af04aa262aa595
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-04 22:14:22 +0530

    Sample function modified

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 Sample () { echo I am a function
+echo "The value of a is $a"
+b=20
 }
 
-Sample
+a=10 ##Here declaring variable a
+Sample ##Here we are calling Sample function and compiled variables in it
+echo "The value of b in main program is $b" ##after calling Sample function above line, value of b compiled 
 
 exit 0

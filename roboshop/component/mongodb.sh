2595ad14c9e5cf115e584dd27412480605f43898
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-04 23:13:02 +0530

    Commit to Mongodb script with new color codes

--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -2,9 +2,9 @@
 
 Status_check(){
   if [ $1 -eq 0 ]; then
-    echo -e "\[ \e[42;1mSuccess\e[0m \]"
+    echo -e "[ \e[42;1mSuccess\e[0m ]"
   else
-    echo -e "\[ \e[41;1mFailure\e[0m \]"
+    echo -e "[ \e[41;1mFailure\e[0m ]"
     exit 2
   fi
 }

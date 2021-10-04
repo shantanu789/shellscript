ac19ece4438acd2b10d0cb4a8492ef4412d9c023
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-04 23:25:36 +0530

    added common function file

---
+++ roboshop/component/common.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+Status_check(){
+  if [ $1 -eq 0 ]; then
+    echo -e "[ \e[42;1mSuccess\e[0m ]"
+  else
+    echo -e "[ \e[41;1mFailure\e[0m ]"
+    exit 2
+  fi
+}
+
+Print(){
+  echo -n -e "$1 - \t\t " ##Tab spaces added just for fancy output
+}
+
+if [ $UID -ne 0 ]; then
+  echo -e "\e[31;1m You should execute this script as a root user\e[0m"
+  exit 1
+fi

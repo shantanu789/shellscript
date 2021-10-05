bdd6bd9bee3e7c12674aabb199be85ccdfc47760
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-05 21:01:53 +0530

    correction in catalogue

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -19,46 +19,46 @@ else
 fi
 Status_check $?
 
-Print "Downloading Catalogue zip\t\t\t"
+Print "Downloading Catalogue content\t\t\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 Status_check $?
 
-Print "Extracting Catalogue zip\t\t\t"
+Print "Extracting Catalogue\t\t\t"
 cd /home/roboshop
-# if [ -e "/tmp/catalogue.zip" ]; then
-#   echo -e "\e[33mZip file Exists,Skipping, already Extracted\e[0m" &>>$LOG
-# else
+rm -rf catalogue
+# if [ -e "/home/roboshop/catalogue" ]; then
+#   echo -e "\e[33mZip file Previous Extract present,removing \e[0m" &>>$LOG
+#   rm -rf catalogue
 #   unzip /tmp/catalogue.zip &>>$LOG
 # fi
 unzip -o /tmp/catalogue.zip &>>$LOG
+mv catalogue-main catalogue
 Status_check $?
 
-Print "Rename Catalogue main Dir\t\t\t"
+# Print "Rename Catalogue main Dir\t\t\t"
 # if [ -d "/home/roboshop/catalogue" ]; then
 #   echo -e "\e[33mSkipping, Directory Exists" &>>$LOG
 # else
-mv -f catalogue-main catalogue
+# mv -f catalogue-main catalogue
 # fi
-
-Status_check $?
-
-cd /home/roboshop/catalogue
+# Status_check $?
 
 Print "Installing npm and NodeJs dependencies\t\t"
+cd /home/roboshop/catalogue
 npm install --unsafe-perm=true &>>$LOG
 Status_check $?
 # NOTE: We need to update the IP address of MONGODB Server in systemd.service file
 # Now, lets set up the service with systemctl.
+chown -R roboshop:roboshop /home/roboshop/
 
 Print "Catalogue Daemon and service Start\t\t\n"
-if [ -e "/etc/systemd/system/catalogue.service" ]; then
-  echo -e "\e[33mCatalogue service file Exists by previous run, skipping moving\e[0m" &>>$LOG
-else {
-  Print "Moving systemd.service to catalogue.service\t\t"
-  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-  Status_check $?
-}
-fi
+# if [ -e "/etc/systemd/system/catalogue.service" ]; then
+#   echo -e "\e[33mCatalogue service file Exists by previous run, skipping moving\e[0m" &>>$LOG
+# else
+Print "Moving systemd.service to catalogue.service\t\t"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+Status_check $?
+# fi
 systemctl daemon-reload
 systemctl start catalogue &>>$LOG
 Status_check $?
@@ -66,5 +66,3 @@ Status_check $?
 Print "Enabling catalogue service\t\t\t"
 systemctl enable catalogue
 Status_check $?
-
-chown -R roboshop:roboshop /home/roboshop/

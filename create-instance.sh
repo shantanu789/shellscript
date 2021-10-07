5a66c53f2d5258136910ba7f31d03e7247ab96bb
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-08 01:37:11 +0530

    Modified Instance create script

--- create-instance.sh
+++ create-instance.sh
@@ -2,7 +2,7 @@
 
 LID="lt-048c498b2ab4c35c3"
 LVER=1
-INSTANCE_NAME="$1"
+INSTANCE_NAME = $1
 
 declare -A INSTANCE_STATE_CODE='([16]="running" [32]="shutting-down" [48]="terminated" [64]="stopping" [80]="stopped")'
 
@@ -15,11 +15,11 @@ fi
 
 INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Code | sed -e 's/"//g')
 
-if [ $(INSTANCE_STATE) == '16' || $(INSTANCE_STATE) == '32' || $(INSTANCE_STATE) == '48' || $(INSTANCE_STATE) == '64' || $(INSTANCE_STATE) == '80' ]; then
+if [ "$(INSTANCE_STATE)" == '16' || "$(INSTANCE_STATE)" == '32' || $(INSTANCE_STATE) == '48' || $(INSTANCE_STATE) == '64' || $(INSTANCE_STATE) == '80' ]; then
   echo -e "\n\e[33m $INSTANCE_NAME is exists and ${INSTANCE_STATE_CODE[$INSTANCE_STATE]}\n"
   exit 1
 fi
 
 echo -e "\t\t\e[32m-----------------Launching Instance-----------------\e[0m\n"
 
-IP=$(aws ec2 --launch-template 'LaunchTemplateId=$LID,Version=$LVER' --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq.Instances[].PrivateIpAddress | sed -e 's/"//g')
+IP=$(aws ec2 --launch-template 'LaunchTemplateId=$LID,Version=$LVER' --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')

e6fbb59730ed66b18a3f73d5452c01b008c7e3c3
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-08 01:58:01 +0530

    Modified Instance create script

--- create-instance.sh
+++ create-instance.sh
@@ -2,21 +2,21 @@
 
 LID="lt-048c498b2ab4c35c3"
 LVER=1
-InstanceNAME="$1"
+InstanceName="$1"
 
 declare -A INSTANCE_STATE_CODE='([16]="running" [32]="shutting-down" [48]="terminated" [64]="stopping" [80]="stopped")'
 
-if [ -z "$InstanceNAME" ]; then
+if [ -z "$InstanceName" ]; then
   echo -e "\n\e[31mInstance Name or Input is missing\e[0m\n"
   exit 1
 fi
 # Getting instance state - running, stopped, terminated
 # instance-state-code - The code for the instance state, as a 16-bit unsigned integer. The high byte is used for internal purposes and should be ignored. The low byte is set based on the state represented. The valid values are 0 (pending), 16 (running), 32 (shutting-down), 48 (terminated), 64 (stopping), and 80 (stopped).
 
-INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$InstanceName" | jq .Reservations[].Instances[].State.Code | sed -e 's/"//g')
+InstanceState="$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$InstanceName" | jq .Reservations[].Instances[].State.Code | sed -e 's/"//g')"
 
-if [ "$(INSTANCE_STATE)" == '16' || "$(INSTANCE_STATE)" == '32' || $(INSTANCE_STATE) == '48' || $(INSTANCE_STATE) == '64' || $(INSTANCE_STATE) == '80' ]; then
-  echo -e "\n\e[33m $InstanceName is exists and ${INSTANCE_STATE_CODE[$INSTANCE_STATE]}\n"
+if [ "$(InstanceState)" == '16' || "$(InstanceState)" == '32' || "$(InstanceState)" == '48' || "$(InstanceState)" == '64' || "$(InstanceState)" == '80' ]; then
+  echo -e "\n\e[33m $InstanceName is exists and ${INSTANCE_STATE_CODE[$InstanceState]}\n"
   exit 1
 fi
 

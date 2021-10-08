bef9a5e55cc892b81dcd3054911843ebc82e8cdc
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-08 15:54:12 +0530

    Modified Instance create script and added record json file

--- create-instance.sh
+++ create-instance.sh
@@ -18,7 +18,7 @@ InstanceState=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$Inst
 echo -e "\t\t\e[32m-----------------Checking Existance of the same Instance-----------------\e[0m\n"
 
 if [[ "$InstanceState" == 16 || "$InstanceState" == 32 || "$InstanceState" == 48 || "$InstanceState" == 64 || "$InstanceState" == 80 ]]; then
-  echo -e "\n\e[33m $InstanceName is exists and ${INSTANCE_STATE_CODE[$InstanceState]}\n"
+  echo -e "\n\e[33m $InstanceName instance exists and is in ${INSTANCE_STATE_CODE[$InstanceState]} state\n"
   exit 1
 else
   echo -e "\n\e[32mCheck Done.No Instance found...\e[0m\n"

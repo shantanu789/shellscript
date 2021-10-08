00fc685926756ad32d66b8828cb5d5e6c22afb6b
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-08 14:52:16 +0530

    Modified Instance create script and added record json file

--- create-instance.sh
+++ create-instance.sh
@@ -13,7 +13,7 @@ fi
 # Getting instance state - running, stopped, terminated
 # instance-state-code - The code for the instance state, as a 16-bit unsigned integer. The high byte is used for internal purposes and should be ignored. The low byte is set based on the state represented. The valid values are 0 (pending), 16 (running), 32 (shutting-down), 48 (terminated), 64 (stopping), and 80 (stopped).
 
-InstanceState="$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$InstanceName" | jq .Reservations[].Instances[].State.Code | sed -e 's/"//g')"
+InstanceState=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$InstanceName" | jq .Reservations[].Instances[].State.Code | sed -e 's/"//g')
 
 echo -e "\t\t\e[32m-----------------Checking Existance of the same Instance-----------------\e[0m\n"
 
@@ -26,7 +26,8 @@ fi
 
 echo -e "\t\t\e[32m-----------------Launching Instance-----------------\e[0m\n"
 
-IP="$(aws ec2 --launch-template 'LaunchTemplateId=$LID,Version=$LVER' --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$InstanceName}]" "ResourceType=instance,Tags=[{Key=Name,Value=$InstanceName}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')"
+IP=${(aws ec2 --launch-template 'LaunchTemplateId=$LID,Version=$LVER' --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$InstanceName}]" "ResourceType=instance,Tags=[{Key=Name,Value=$InstanceName}]") | jq .Instances[].PrivateIpAddress | sed -e 's/"//g'}
 
 sed -e "s/INSTANCE_NAME/$InstanceName/" -e "s/INSTANCE_IP/$IP/" record.json >/tmp/record.json
+
 aws route53 change-resource-record-sets --hosted-zone-id Z04674552UCKELJX08IB3 --change-batch file:///tmp/record.json | jq
